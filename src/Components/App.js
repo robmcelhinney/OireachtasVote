d8eabcb2376d4c54d893d847b90a775eb6841fde
robmcelhinney <robmcelhinney@hotmail.com>
2019-11-22 23:39:41 +0000

    Reformat files. Fix exclude button. Add finishing touches.
    Split App.js into app and table. Table.js is a stateless react component
    for the react-table and its filtering.
    App.js contains everything else. App passes the table data to Table.js
    as a prop as the exclude cabinet button is in App.js.
    Edit page title.
    Add footer.
    Add favicon.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,297 +1,95 @@
-import React, {useEffect, useState} from 'react';
-import ReactTable from 'react-table';
+import React from 'react';
 import 'react-table/react-table.css';
-import members from '../members.json';
 import info from '../info.json';
-import Select from 'react-select';
-import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
-import wNumb from 'wnumb';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormGroup from '@material-ui/core/FormGroup';
+import Table from './Table';
+import members from '../members.json';
 
-function filterCaseInsensitive(filter, row) {
-	const id = filter.pivotId || filter.id;
-	if (["party", "firstName", "lastName", "fullName"].includes(id)) {
-		return (
-			row[id] !== undefined ?
-				String(row[id].toLowerCase()).includes(filter.value.toLowerCase())
-				: true
-		);
-	}
-	else if (["votes", "percent"].includes(id)) {
-		return (
-			row[id] !== undefined ?
-				String(row[id]).includes(filter.value)
-				: true
-		);
-	}
-}
 
-let data = members;
-
-const App = () => {
-	const [percentLower, setPercentLower] = useState(
-		0
-	);
-	const [percentHigher, setPercentHigher] = useState(
-		100
-	);
-	const [voteLower, setVoteLower] = useState(
-		0
-	);
-	const [voteHigher, setVoteHigher] = useState(
-		info['totalVotes']
-	);
-	const [widthHide, setWidthHide] = useState(
-		false
-	);
-	// const [excludeCeannComhairle, setExcludeCeannComhairle] = useState(
-	// 	false
-	// );
-	const [excludeTDs, setExcludeTDs] = useState(
-		false
-	);
-
-	function handlePercentRange(event, onChange) {
-		setPercentLower([event[0]]);
-		setPercentHigher([event[1]]);
-		onChange(event)
-	}
-	function handleVoteRange(event, onChange) {
-		setVoteLower([event[0]]);
-		setVoteHigher([event[1]]);
-		onChange(event)
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			excludeCabinet: false,
+			excludeTaoiseach: false,
+			data: members,
+		};
 	}
 
-	const party_options = info['parties'];
-
-	const resize = () => {
-		let smallerWidth = (window.innerWidth <= 768);
-		if (smallerWidth !== widthHide) {
-			setWidthHide(smallerWidth);
-		}
-		if (widthHide) {
-			partyWidth = "100";
-			nameWidth = "100";
-		}
+	handleChecked = () => {
+		this.setState({excludeCabinet: !this.state.excludeCabinet});
+		const newSelection = !this.state.excludeCabinet;
+		this.handleAlterMembers(newSelection);
 	};
 
-	useEffect(() => {
-		window.addEventListener("resize", resize);
-		// console.log("data 1: ", data);
-		// console.log("excludeTDs: ", excludeTDs);
-		if (excludeTDs) {
-			data = [];
+	handleAlterMembers = (newSelection) => {
+		if (newSelection) {
+			let data = [];
 			members.forEach(
 				member => {
 					if (!member["office"]) {
 						data.push(member)
 					}}
 			);
+			this.setState({data: data});
 		}
 		else {
-			data = members;
-		}
-		// console.log("data 2: ", data);
-		return () => {
-			window.removeEventListener("resize", resize);
-		};
-	}, [resize]);
-
-	let partyWidth = "250";
-	let nameWidth = "150";
-
-	const pictureColumn = {
-		id: "picture",
-		accessor: d => {
-			return <div><a href={"https://www.oireachtas.ie/en/members/member/"
-			+ d.member_id}><img alt={"td"}
-								className={"member__avatar"}
-								src={"https://data.oireachtas.ie/ie/oireachtas/member/id/"
-								+ d.member_id + "/image/thumb"}/></a></div>
-		},
-		filterable: false,
-		maxWidth: "100",
-	};
-
-	const voteColumn = {
-		id: "Votes",
-		Header: "Votes",
-		accessor: "votes",
-		maxWidth: "150",
-		filterMethod: (filter, row) => {
-			const id = filter.pivotId || filter.id;
-			return (voteLower <= Number(row[id])
-				&& voteHigher >= Number(row[id]))
-		},
-
-		Filter: ({ filter, onChange }) =>
-			<div className={"nouislider_div"} >
-				<Nouislider
-					className={"nouislider"}
-					range={{ min: 0, max: info['totalVotes'] }}
-					start={[voteLower, voteHigher]}
-					connect={true}
-					step={10}
-					value={filter ? filter.value : 'all'}
-					style={{width:'70%', marginLeft:'15%'}}
-
-					onChange={event => handleVoteRange(event, onChange)}
-					tooltips={[ wNumb({ decimals: 0 }), wNumb({ decimals: 0 }) ]}
-				/></div>,
-	};
-
-	const fullNameColumn = {
-		Header: "Full Name",
-		id: "fullName",
-		accessor: d => {
-			return d.firstName + " " + d.lastName
-		},
-		maxWidth: "150",
-	};
-
-	const firstNameColumn = {
-		Header: "First Name",
-		accessor: "firstName",
-		maxWidth: nameWidth,
-	};
-
-	const lastNameColumn = {
-		Header: "Last Name",
-		accessor: "lastName",
-		maxWidth: nameWidth,
-	};
-
-	const columns = [
-		{
-			Header: "Party",
-			accessor: "party",
-			maxWidth: partyWidth,
-			filterMethod: (filter, row) => {
-				const id = filter.pivotId || filter.id;
-				if (filter['value'] && filter['value'].length !== 0) {
-					let parties = [];
-					filter['value'].forEach(
-						element =>
-							parties.push(element.label)
-					);
-					return (parties.includes(String(row[id])))
-				}
-				return true;
-			},
-			Filter: ({ filter, onChange }) => {
-				return (
-					<Select
-						defaultValue={[]}
-						isMulti
-						name="colors"
-						options={party_options}
-						className="basic-multi-select"
-						classNamePrefix="select"
-						menuPortalTarget={document.body}
-						onChange={event => onChange(event)}
-						value={filter ? filter.value : 'all'}
-					/>
-				);
+			if (this.state.data !== members) {
+				this.setState({data: members})
 			}
-		},
-		{
-			Header: "Percent",
-			id: "Percent",
-			accessor: d => {
-				return d.percentVotes
-			},
-			filterMethod: (filter, row) => {
-				const id = filter.pivotId || filter.id;
-				return (percentLower <= Number(row[id])
-					&& percentHigher >= Number(row[id]))
-			},
-
-			Filter: ({ filter, onChange }) =>
-				<div className={"nouislider_div"}>
-					<Nouislider
-						className={"nouislider"}
-						range={{ min: 0, max: 100 }}
-						start={[percentLower, percentHigher]}
-						connect={true}
-						step={1}
-						value={filter ? filter.value : 'all'}
-						style={{width:'90%'}}
-						onChange={event => handlePercentRange(event, onChange)}
-						pips={{
-							mode: "count",
-							values: 2,
-						}}
-						tooltips={[ wNumb({ decimals: 0 }), wNumb({ decimals: 0 }) ]}
-					/></div>,
-		},
-	];
-
-	if (window.innerWidth <= 768) {
-		columns.unshift({
-			Header: "Name",
-			columns: [
-				fullNameColumn
-			]
-		});
-	}
-	else {
-		columns.unshift({
-			Header: "Name",
-			columns: [
-				firstNameColumn,
-				lastNameColumn
-			]
-		});
-		columns.unshift(pictureColumn);
-		columns.push(voteColumn);
-	}
-
-	// const handleCCChecked = () => {
-	// 	setExcludeCeannComhairle(!excludeCeannComhairle)
-	// };
-	const handleTDsChecked = () => {
-		setExcludeTDs(!excludeTDs)
+		}
 	};
 
-	return (
-		<div id={"maincontent"} className={"container"}>
-			<div id={"headerInfo"}>
-				<h1 className={"mainHeader"}>TD Voting Record - {info.currentDail}</h1>
-				<p>info <span id={"should_be"}>**should be**</span> accurate as of {info.dateCreated}</p>
-				<FormGroup row>
-					{/*<FormControlLabel*/}
-						{/*control={*/}
-							{/*<Checkbox checked={excludeCeannComhairle} onChange={handleCCChecked} value={excludeCeannComhairle} />*/}
-						{/*}*/}
-						{/*label="Exclude Ceann Comhairle"*/}
-					{/*/>*/}
-					<FormControlLabel
-						control={
-							<Checkbox checked={excludeTDs} onChange={handleTDsChecked} value={excludeTDs} />
+	render() {
+		return (
+			<>
+			<div id={"maincontent"} className={"container"}>
+				<div id={"headerInfo"}>
+					<h1 className={"mainHeader"}>TD Voting Record - {info.currentDail}</h1>
+					<p><span className={"should_be"}>**should be**</span> Accurate as of {info.dateCreated}</p>
+					<FormGroup row>
+						<FormControlLabel
+							control={
+								<Checkbox checked={this.state.excludeCabinet}
+										  onChange={() =>
+										  this.handleChecked()}
+										  value={this.state.excludeCabinet} />
 						}
-						label="Exclude TDs & Taoiseach"
-					/>
-				</FormGroup>
+							label="Exclude Cabinet Members"
+						/>
+						{/*<FormControlLabel*/}
+							{/*control={*/}
+								{/*<Checkbox checked={this.state.excludeTaoiseach}*/}
+										  {/*onChange={() =>*/}
+											  {/*this.handleChecked("taoiseach")}*/}
+										  {/*value={this.state.excludeTaoiseach} />*/}
+							{/*}*/}
+							{/*label="Exclude Taoiseach"*/}
+						{/*/>*/}
+					</FormGroup>
+				</div>
+				<Table members={this.state.data}/>
+				<p>Plan to run the web scraper at least once a week to keep the
+					info relevant.</p>
+				<p>If you have any ideas please let me know on
+					Twitter/Github below, i.e. should I keep TDs no longer
+					in the Dáil (Current MEPs) or should I automatically
+					remove the Ceann-Comhairle <span className={"should_be"}>
+					blame https://data.oireachtas.ie/</span></p>
 			</div>
-			<ReactTable
-				data={data}
-				columns={columns}
-				filterable
-				className="-striped -highlight"
-				defaultSortDesc={true}
-				defaultSorted={[{
-					id: 'Percent',
-					desc: false
-				}]}
-				defaultFilterMethod={(filter, row) => filterCaseInsensitive(filter, row)}
-				defaultPageSize={15}
-				pageSizeOptions={[5, 10, 15, 20, 50, 100]}
-			/>
-		</div>
-	);
-};
+			<div id={"footer"}
+				style={{color: 'black'}}>
+				Any queries/requests, contact me @<a
+				href={"https://twitter.com/RMcElhinney"}>Rob McElhinney
+				</a> | Code Viewable @<a
+				href={"https://github.com/robmcelhinney/oireachtasVote"}>github</a>
+			</div>
+			</>
+		);
+	}
+}
 
 export default App;
