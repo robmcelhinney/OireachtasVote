8acb3219fe15b2aadd44fc9a4100031d74c282dd
robmcelhinney <robmcelhinney@hotmail.com>
2019-11-23 00:10:53 +0000

    Updates

---
+++ precache-manifest.7b65382172bd8078d037dd2f1e59a6e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9526c3e617e7e723ccd3a1c0c96b0b97",
+    "url": "/OireachtasVote/index.html"
+  },
+  {
+    "revision": "e20a5ac2d3b57c8b2a39",
+    "url": "/OireachtasVote/static/css/2.bed5b423.chunk.css"
+  },
+  {
+    "revision": "1c87327b4c1a539912c3",
+    "url": "/OireachtasVote/static/css/main.f8e8b0ea.chunk.css"
+  },
+  {
+    "revision": "e20a5ac2d3b57c8b2a39",
+    "url": "/OireachtasVote/static/js/2.169f8d1f.chunk.js"
+  },
+  {
+    "revision": "1c87327b4c1a539912c3",
+    "url": "/OireachtasVote/static/js/main.c928909b.chunk.js"
+  },
+  {
+    "revision": "bb6349a7a789c46f2d58",
+    "url": "/OireachtasVote/static/js/runtime-main.282d28eb.js"
+  },
+  {
+    "revision": "410a26014ce7e25824ae18349d2c92d3",
+    "url": "/OireachtasVote/static/media/Quicksand-Bold.410a2601.ttf"
+  },
+  {
+    "revision": "9a891411c3856f2db214f3507349a424",
+    "url": "/OireachtasVote/static/media/Quicksand-Regular.9a891411.ttf"
+  }
+]);
