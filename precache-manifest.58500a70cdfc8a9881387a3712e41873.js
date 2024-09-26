1908b4a2ddee4b8e5368b35e72b4ffda1422d9fe
Robert McElhinney <robmcelhinney@hotmail.com>
2024-09-26 20:34:55 +0100

    Updates

---
+++ precache-manifest.58500a70cdfc8a9881387a3712e41873.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923e9f0e8853a8609d23099c3b8a5a72",
+    "url": "/OireachtasVote/index.html"
+  },
+  {
+    "revision": "db3afcf757a2f6adde94",
+    "url": "/OireachtasVote/static/css/2.0f68107e.chunk.css"
+  },
+  {
+    "revision": "e7458544c2f33e132cb6",
+    "url": "/OireachtasVote/static/css/main.bb4f76a5.chunk.css"
+  },
+  {
+    "revision": "db3afcf757a2f6adde94",
+    "url": "/OireachtasVote/static/js/2.0f122dc8.chunk.js"
+  },
+  {
+    "revision": "d3d13aeee4cbf3bcb781ed3fbba00989",
+    "url": "/OireachtasVote/static/js/2.0f122dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7458544c2f33e132cb6",
+    "url": "/OireachtasVote/static/js/main.b184be6d.chunk.js"
+  },
+  {
+    "revision": "e9e9498a33c3c2057f94",
+    "url": "/OireachtasVote/static/js/runtime-main.5838e0e9.js"
+  },
+  {
+    "revision": "410a26014ce7e25824ae18349d2c92d3",
+    "url": "/OireachtasVote/static/media/Quicksand-Bold.410a2601.ttf"
+  },
+  {
+    "revision": "9a891411c3856f2db214f3507349a424",
+    "url": "/OireachtasVote/static/media/Quicksand-Regular.9a891411.ttf"
+  },
+  {
+    "revision": "6aad34a25c9f20dd6c82a86874fda9b0",
+    "url": "/OireachtasVote/static/media/user.6aad34a2.svg"
+  }
+]);
